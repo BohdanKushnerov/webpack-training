d162ec4f9eeec0f7aa40987157728d534b857c09
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-01-28 00:35:36 +0200

    Add files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -11,19 +13,33 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [
+          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
-      }
+        use: ['babel-loader'],
+      },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({template: 'src/index.html'})],
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new MiniCssExtractPlugin({filename: 'styles.css'}),
+    new CleanWebpackPlugin(),
+  ],
   devServer: {
     static: {
-        directory: path.join(__dirname, '/')
+      directory: path.join(__dirname, '/'),
     }
-},
+  },
 };
